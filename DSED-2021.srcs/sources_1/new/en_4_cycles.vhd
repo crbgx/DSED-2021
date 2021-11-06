28176d70f13efc408d34cb0d00d3bd53fc3ed672
efrenbg1 <efren@boyarizo.es>
2021-11-06 16:23:40 +0100

    Añadido simulación real del en_4_cycles

--- DSED-2021.srcs/sources_1/new/en_4_cycles.vhd
+++ DSED-2021.srcs/sources_1/new/en_4_cycles.vhd
@@ -13,16 +13,25 @@ end en_4_cycles;
 architecture Behavioral of en_4_cycles is
 
 signal count, count_next : unsigned(1 downto 0) := (others => '0');
-signal reset_hold : std_logic := '0';
+signal reset_hold, clk : std_logic := '0';
+component clk_wiz_0 port (
+    clk_out1: out std_logic;
+    clk_in1: in std_logic);
+end component;
 
 begin
 
-process(clk_12megas, reset)
+UUT : clk_wiz_0 port map(
+    clk_out1 => clk,
+    clk_in1 => clk_12megas
+);
+
+process(clk, reset)
 begin
     if reset='1' then
         count <= (others => '0');
         reset_hold <= '1';
-    elsif rising_edge(clk_12megas) then
+    elsif rising_edge(clk) then
         count <= count_next;
         reset_hold <= '0';
     end if;
@@ -33,6 +42,5 @@ en_2_cycles <= not count(0) and not reset_hold;
 en_4_cycles <= (not count(1)) and count(0); 
 count_next <= count + 1 when reset_hold='0' else
               (others => '0');
-
 
 end Behavioral;
