a4b422c4d817e76c70f07f2d45fdcaf44cd38888
efrenbg1 <efren@boyarizo.es>
2021-11-13 12:51:13 +0100

    Terminado FSMD_microphone?

--- DSED-2021.srcs/sources_1/new/FSMD_microphone.vhd
+++ DSED-2021.srcs/sources_1/new/FSMD_microphone.vhd
@@ -17,91 +17,106 @@ architecture Behavioral of FSMD_microphone is
 type state_type is (start, s0, s1, s2);
 signal state, state_next : state_type := start;
 signal dato1, dato2, dato1_next, dato2_next : unsigned(7 downto 0) := (others => '0');
-signal primer_ciclo : std_logic := '0';
+signal primer_ciclo, primer_ciclo_next : std_logic := '0';
 signal cuenta, cuenta_next : unsigned(8 downto 0) := (others => '0');
 
 begin
 
-process(clk_12megas)
+process(clk_12megas, reset)
 begin
     if rising_edge(clk_12megas) then
-        state <= state_next;
-        cuenta <= cuenta_next;
+        if reset='1' then
+            state <= start;
+        else
+            state <= state_next;
+            cuenta <= cuenta_next;
+            dato1 <= dato1_next;
+            dato2 <= dato2_next;
+            primer_ciclo <= primer_ciclo_next;
+        end if;
     end if;
 end process;
 
-process(state, cuenta, reset, micro_data)
+process(state, cuenta)
 begin
     state_next <= state;
+    case state is
+        when start =>
+            state_next <= s0;
+        when s0 =>
+            if cuenta > 104 then
+                if cuenta > 254 then
+                    state_next <= s2;
+                elsif cuenta < 150 then
+                    state_next <= s1;
+                end if;
+            end if;
+        when s1 =>
+            if cuenta = 149 then
+                state_next <= s0;
+            end if;
+        when others =>
+            if cuenta = 299 then
+                state_next <= s0;
+            end if;
+    end case;
+end process;
+
+process(state_next, micro_data, dato1, dato2, primer_ciclo)
+begin
     cuenta_next <= cuenta;
     dato1_next <= dato1;
     dato2_next <= dato2;
-    if reset='1' then
-        state_next <= start;
-    else
-        case state is
-            when start =>
+    case state_next is
+        when start =>
+            cuenta_next <= (others => '0');
+            dato1_next <= (others => '0');
+            dato2_next <= (others => '0');
+            primer_ciclo_next <= '0';
+        when s0 =>
+            if cuenta=299 then
                 cuenta_next <= (others => '0');
-                dato1 <= (others => '0');
-                dato2 <= (others => '0');
-                primer_ciclo <= '0';
-                state_next <= s0;
-            when s0 =>
+            else
                 cuenta_next <= cuenta + 1;
-                if micro_data = '1' then
-                    dato1 <= dato1 + 1;
-                    dato2 <= dato2 + 1;
-                end if;
-                if cuenta > 104 then
-                    if cuenta > 254 then
-                        state_next <= s2;
-                    elsif cuenta < 150 then
-                        state_next <= s1;
-                    end if;
-                end if;
-            when s1 =>
+            end if;
+            if cuenta=149 then
+                dato2_next <= (others => '0');
+            elsif micro_data = '1' then
+                dato2_next <= dato2 + 1;
+            end if;
+            if cuenta=256 then
+                dato1_next <= (others => '0');
+            elsif micro_data = '1' then
+                dato1_next <= dato1 + 1;
+            end if;
+        when s1 =>
+            cuenta_next <= cuenta + 1;
+            if micro_data = '1' then
+                dato1_next <= dato1 + 1;
+            end if;
+            if primer_ciclo='1' and cuenta = 106 then
+                sample_out <= std_logic_vector(dato2);
+                sample_out_ready <= enable_4_cycles;
+            else
+                sample_out_ready <= '0';
+            end if;
+        when others =>
+            if cuenta = 299 then
+                cuenta_next <= (others => '0');
+                primer_ciclo_next <= '1';
+            else
                 cuenta_next <= cuenta + 1;
-                if micro_data = '1' then
-                    dato1 <= dato1 + 1;
-                end if;
-                if primer_ciclo='1' and cuenta = 106 then
-                    sample_out <= std_logic_vector(dato2);
-                    dato2 <= (others => '0');
-                    sample_out_ready <= enable_4_cycles;
-                else
-                    sample_out_ready <= '0';
-                end if;
-                if cuenta > 148 then
-                    state_next <= s0;
-                end if;
-            when others =>
-                if micro_data = '1' then
-                    dato2 <= dato2 + 1;                
-                end if;
-                if cuenta = 299 then
-                    cuenta_next <= (others => '0');
-                    primer_ciclo <= '1';
-                    state_next <= s0;
-                else
-                    cuenta_next <= cuenta +1;
-                end if;
-                if cuenta = 256 then
-                    --sample_out <= std_logic_vector(dato1);
-                    dato1_next <= (others => '0');
-                    sample_out_ready <= enable_4_cycles;
-                else
-                    sample_out_ready <= '0';
-                end if;
-                --if cuenta_next = 0 then
-                  --  state_next <= s0;
-                --end if;
-        end case;
-    end if;
+            end if;
+            if micro_data = '1' then
+                dato2_next <= dato2 + 1;                
+            end if;
+            if cuenta = 256 then
+                sample_out <= std_logic_vector(dato1);
+                sample_out_ready <= enable_4_cycles;
+            else
+                sample_out_ready <= '0';
+            end if;
+    end case;
 end process;
-
-
---OutputLogic
-sample_out <= std_logic_vector(dato1) when cuenta >= 256,
-                else std_logic_vector(dato2);
                 
 end Behavioral;
