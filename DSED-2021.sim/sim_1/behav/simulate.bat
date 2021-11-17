d368121835d32ad659c5bca6eff2d04a04fc09d3
efrenbg1 <efren@boyarizo.es>
2021-11-17 09:35:56 +0100

    Limpiado directorio de archivos no necesarios

--- DSED-2021.sim/sim_1/behav/simulate.bat
+++ DSED-2021.sim/sim_1/behav/simulate.bat
@@ -1,6 +1,6 @@
 @echo off
 set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xsim pwm_tb_behav -key {Behavioral:sim_1:Functional:pwm_tb} -tclbatch pwm_tb.tcl -view D:/DSED-2021/FSMD_microphone_tb_behav.wcfg -log simulate.log
+call %xv_path%/xsim pwm_tb_behav -key {Behavioral:sim_1:Functional:pwm_tb} -tclbatch pwm_tb.tcl -log simulate.log
 if "%errorlevel%"=="0" goto SUCCESS
 if "%errorlevel%"=="1" goto END
 :END
