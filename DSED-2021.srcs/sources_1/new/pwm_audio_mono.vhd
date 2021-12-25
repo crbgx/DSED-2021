f7818c5a7ba346a6617373b92a84147b11235ece
efrenbg1 <efren@boyarizo.es>
2021-12-25 12:20:45 +0100

    Comentado generador pwm

--- DSED-2021.srcs/sources_1/new/pwm_audio_mono.vhd
+++ DSED-2021.srcs/sources_1/new/pwm_audio_mono.vhd
@@ -16,11 +16,14 @@ end pwm;
 
 architecture Behavioral of pwm is
 
+-- Señales de cuenta interna
 signal cuenta, cuenta_next : unsigned(8 downto 0) := (others => '0');
+-- Buffer para la señal de salida pwm
 signal buf_reg, buf_next : std_logic := '0';
 
 begin
--- register & output buffer
+
+
 process(clk_12megas, reset, en_2_cycles)
 begin
     if (reset='1') then
@@ -32,11 +35,14 @@ begin
     end if;
 end process;
 
+-- Incrementar en 1 la cuenta o resetear a 0 si se llega al valor máximo
 cuenta_next <= cuenta + 1 when cuenta < 299 else
                (others => '0');
+-- La salida pwm se puede obtener directamente de comparar cuenta con el valor en sample_in
 buf_next <= '1' when cuenta_next < unsigned(sample_in) else
             '0';
 pwm_pulse <= buf_reg;
+-- Sample request solo se necesita mantener activo durante un ciclo de reloj cuando la cuenta llega al final
 sample_request <= '1' when cuenta=299 and en_2_cycles='1' else
                   '0';
 
